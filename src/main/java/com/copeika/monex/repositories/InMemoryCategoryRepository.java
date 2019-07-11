b51138f8f29e76f597e6cc8e825d8cbb64f695c2
snt_alex <alexgolikov5@mail.ru>
2019-07-11 15:18:49 +0700

    not final at all

--- src/main/java/com/copeika/monex/repositories/InMemoryCategoryRepository.java
+++ src/main/java/com/copeika/monex/repositories/InMemoryCategoryRepository.java
@@ -22,6 +22,9 @@ public class InMemoryCategoryRepository implements CategoryRepository {
 
     @Override
     public Category addCategory(String name) {
+        if (!(name.trim().length() > 0)){
+            throw new NotValidValue();
+        }
         if (categoryCache.containsKey(name)) {
             throw new AlreadyExistException();
         }
@@ -34,6 +37,9 @@ public class InMemoryCategoryRepository implements CategoryRepository {
 
     @Override
     public void deleteCategory(String name) {
+        if (!(name.trim().length() > 0)){
+            throw new NotValidValue();
+        }
         if (!categoryCache.containsKey(name)) {
             throw new NotFoundException();
         }
@@ -42,9 +48,16 @@ public class InMemoryCategoryRepository implements CategoryRepository {
 
     @Override
     public Category renameCategory(String name, String after_name) {
+        if (!(name.trim().length() > 0) || !(after_name.trim().length() > 0)){
+            throw new NotValidValue();
+        }
         if (!categoryCache.containsKey(name)) {
             throw new NotFoundException();
         }
+        if (categoryCache.containsKey(after_name)) {
+            throw new AlreadyExistException();
+        }
+
         Category category = categoryCache.get(name);
         category.setName(after_name);
         categoryCache.remove(name);
@@ -54,6 +67,9 @@ public class InMemoryCategoryRepository implements CategoryRepository {
 
     @Override
     public Category fetchCategory(String name) {
+        if (!(name.trim().length() > 0)){
+            throw new NotValidValue();
+        }
         if (!categoryCache.containsKey(name)) {
             throw new NotFoundException();
         }
@@ -62,6 +78,9 @@ public class InMemoryCategoryRepository implements CategoryRepository {
 
     @Override
     public Category setLimit(String name, Integer limit) {
+        if (!(name.trim().length() > 0)){
+            throw new NotValidValue();
+        }
         if(!(limit>=1 && limit <= 10000000)) {
             throw new NotValidValue();
         }
@@ -76,6 +95,9 @@ public class InMemoryCategoryRepository implements CategoryRepository {
 
     @Override
     public Category addMonetaryExpenditures(String name, Integer money_expenditures) {
+        if (!(name.trim().length() > 0)){
+            throw new NotValidValue();
+        }
         if(!(money_expenditures>=1 && money_expenditures <= 10000000)) {
             throw new NotValidValue();
         }
@@ -91,6 +113,9 @@ public class InMemoryCategoryRepository implements CategoryRepository {
 
     @Override
     public Category refreshMonetaryExpenditures(String name) {
+        if (!(name.trim().length() > 0)){
+            throw new NotValidValue();
+        }
         if (!categoryCache.containsKey(name)) {
             throw new NotFoundException();
         }
